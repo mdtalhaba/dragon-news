b5fbd730eb9584301ad4b3d9f40bf2c49713d005
mdtalhaba <jubairt35@gmail.com>
2022-10-20 13:04:28 -0700

    Data Loader Add

--- src/pages/Shared/RightSideNav/RightSideNav.js
+++ src/pages/Shared/RightSideNav/RightSideNav.js
@@ -14,13 +14,13 @@ const RightSideNav = () => {
                 <Button variant='outline-dark'> <FaGithub></FaGithub> Login With Github</Button>
             </ButtonGroup>
             <div>
-                <h5>Find us on</h5>
+                <h5 className='mt-4'>Find us on</h5>
                 <ListGroup>
-                    <ListGroup.Item className='mb-2'><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
-                    <ListGroup.Item className='mb-2'><FaWhatsapp></FaWhatsapp> WhatsApp</ListGroup.Item>
-                    <ListGroup.Item className='mb-2'><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
-                    <ListGroup.Item className='mb-2'><FaLinkedin></FaLinkedin> Linkedin</ListGroup.Item>
-                    <ListGroup.Item className='mb-2'><FaInstagram></FaInstagram> Instagram</ListGroup.Item>
+                    <ListGroup.Item className='mb-2 ps-4'><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2 ps-4'><FaWhatsapp></FaWhatsapp> WhatsApp</ListGroup.Item>
+                    <ListGroup.Item className='mb-2 ps-4'><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2 ps-4'><FaLinkedin></FaLinkedin> Linkedin</ListGroup.Item>
+                    <ListGroup.Item className='mb-2 ps-4'><FaInstagram></FaInstagram> Instagram</ListGroup.Item>
                 </ListGroup>
             </div>
             <div>
